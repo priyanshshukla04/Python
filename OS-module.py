9332126e75fa3b2a67eed9cb43e01dbccff4daa4
Priyansh Shukla <60940500+priyanshshukla04@users.noreply.github.com>
2021-02-08 12:03:58 +0530

    Add files via upload

---
+++ OS-module.py
@@ -0,0 +1,14 @@
+import os
+# print(dir(os))
+# print(os.getcwd())
+# os.chdir("/Users/priyanshshukla/PycharmProjects")
+# print(os.getcwd())
+# f = open("priyansh.txt")
+# print(os.listdir("/Users/priyanshshukla/PycharmProjects"))
+# os.makedirs("This/that")
+# os.rename("priyansh.txt", "anshul.txt")
+# print(os.environ.get('HOME'))
+# print(os.path.join("C:/", "/priyansh.txt"))
+
+# print(os.path.exists("C://Program Files2"))
+# print(os.path.isfile("C://Program Files"))

0b218c93da90a98170a80e4b2271e30247897fd3
Priyansh Shukla <60940500+priyanshshukla04@users.noreply.github.com>
2021-02-08 11:58:29 +0530

    Add files via upload

---
+++ Lambda_Function.py
@@ -0,0 +1,20 @@
+#def sum(x,y):
+#    return x+y
+#the above statement is same as below
+#sum = lambda (x,y):x+y
+#print(sum(9,4))
+
+
+
+
+# def elements(a):
+#     return a[1]
+# a = [[1,3],[1,2],[1,4]]
+# a.sort(key=elements)
+# print(a)
+#the above statement is same as below
+# a = [[1,3],[1,2],[1,4]]
+# a.sort(key=lambda x : x[1])
+# print(a)
+
+

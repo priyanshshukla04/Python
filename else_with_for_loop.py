107158409fa70afda59a4308544585f689cab0b2
Priyansh Shukla <60940500+priyanshshukla04@users.noreply.github.com>
2021-02-08 11:04:41 +0530

    Add files via upload

---
+++ else_with_for_loop.py
@@ -0,0 +1,13 @@
+item = ["anshul","saumya","vansh","shubhansh"]
+for i in item:
+    print(i)
+else:
+    print("For loop1 executed successfully")
+#other use case
+item1 = ["anshul", "saumya", "vansh", "shubhansh"]
+for i in item:
+    # if i=="anshul":
+    if i=="a":
+       break
+else:
+    print("For loop2 executed successfully")

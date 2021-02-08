a1599683ec38c3347680ddc522bfd1cf6ae93daa
Priyansh Shukla <60940500+priyanshshukla04@users.noreply.github.com>
2021-02-08 12:02:20 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,45 @@
+#Arithmetic operators
+print("5 + 6 is ",5+6)
+print("5 - 6 is ",5-6)
+print("5 * 6 is ",5*6)
+print("5 / 6 is ",5/6)
+print("15 // 6 is ",15//6)
+print("5 ** 3 is ",5**3)
+print("12 % 6 is ",12%6)
+
+#Assignment opertors
+x=5
+print(x)
+x%=7      #x+=7   x/=7   x-=7
+print(x)
+
+#Comparison operators
+i=5
+print(i==5)
+print(i!=5)
+print(i>5)
+print(i>=5)
+print(i<=5)
+
+#Logical operators
+a=True
+b=False
+print(a and b)
+print(a or b)
+
+#Identity operators
+print(a is b)
+print(a is not b)
+
+#Membership operators
+list=[1,2,33,4,5,6]
+print(1 in list)
+print(32 in list)
+
+#Bitwise operators
+#0 - 00
+#1 - 01
+#2 - 10
+#3 - 11
+print(0 & 1)
+print(0 | 1)

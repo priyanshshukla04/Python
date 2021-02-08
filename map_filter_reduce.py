05dd08cb36375cc4294b80ea4a0d976ad2863733
Priyansh Shukla <60940500+priyanshshukla04@users.noreply.github.com>
2021-02-08 11:19:53 +0530

    Add files via upload

---
+++ map_filter_reduce.py
@@ -0,0 +1,50 @@
+#********************MAP************
+#lis1 = [1,2,3,4]
+# num = list(map(lambda x : x**2,lis1))
+# print(num)
+
+# lis2 = ["1","2","3","4"]
+# num2 = list(map(int,lis2))
+#print(num2)
+# number = 0
+# for i in range(len(lis2)+1):
+#     i = int(i)
+#     number = number+i
+# print(number)
+# def sq(a):
+#     return a*a
+# numb = list(map(sq,lis1))
+# print(numb)
+# def square(y):
+#     return y*y
+# def cube(z):
+#     return z*z*z
+# listtt = [square, cube]
+# for i in range(5):
+#     ans = list(map(lambda x:x(i),listtt))
+#     print(ans)
+
+
+
+
+
+#******************FILTER************
+# a = [1,2,3,4,5,6]
+# b = [1,3,5,7,9,11]
+# num3 = list(filter(lambda x:x in a,b))
+# print(num1)
+# lis3 = [1,2,3,4,5,6,7,8]
+# def is_greater_5(s):
+#     return s>5
+# numbe = list(filter(is_greater_5,lis3))
+# print(numbe)
+
+
+# #*****************REDUCE*************
+# from functools import reduce
+# lis2 = [1,2,3,4,5]
+# num4 = reduce(lambda x,y :x+y,lis2)
+#print(num2)
+
+
+

2263701c4effe614c3ec56371842c10f8d87b3eb
Priyansh Shukla <60940500+priyanshshukla04@users.noreply.github.com>
2021-02-08 11:44:17 +0530

    Add files via upload

---
+++ Apni_Dictionary.py
@@ -0,0 +1,5 @@
+'exercise 1 youtube codewithharry'
+d={"abandon" :"cease to support or look after","magnificent":"extremely beautiful","flamboyant":"tending to attract attention because of their exuberance, confidence, and stylishness","ostentatious":"designed to impress"}
+word=input("enter the word: ")
+print(d[word])
+

58a36335902bf62ef5cdf9890a6861587f784c8f
Priyansh Shukla <60940500+priyanshshukla04@users.noreply.github.com>
2021-02-08 12:28:14 +0530

    Add files via upload

---
+++ File_handling1.py
@@ -0,0 +1,23 @@
+#open("filename" ,"mode")
+#open("myfile.txt")
+#  f=open("myfile.txt," "w")
+#f = open("myfile.txt", "r")
+#f.read(2); #Here, you will get the first two characters of the file.
+#f=open("myfile.txt","r");
+#f.readlines() #Returns a list object
+#f = open("harry.txt", "rt")
+#print(f.readlines())
+# print(f.readline())
+# print(f.readline())
+# print(f.readline())
+# content = f.read()
+#
+# for line in f:
+#     print(line, end="")
+# print(content)
+# content = f.read(34455)
+# print("1", content)
+#
+# content = f.read(34455)
+# print("2", content)
+#f.close()
